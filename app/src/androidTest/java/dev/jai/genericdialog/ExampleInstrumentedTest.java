bfbc805b04b89b318a0acf1cc77388c43e70a1f0
Jaidev Naik <jaidev.naik@transerve.com>
2019-05-03 18:11:50 +0530

    -- package name changed

--- app/src/androidTest/java/dev/jai/genericdialog/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/dev/jai/genericdialog/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.transerve.genericdialog2;
+package dev.jai.genericdialog;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.transerve.genericdialog2.test", appContext.getPackageName());
+        assertEquals("com.transerve.genericdialog", appContext.getPackageName());
     }
 }
